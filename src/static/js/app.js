261cb82b811032db5ac9ee66b6a56153be4c51f9
fphilipp87 <federico.philipp@gmail.com>
2022-10-11 20:13:53 +0000

    change2

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Change 1 !</p>
+                <p className="text-center">Change 2 !</p>
             )}
             {items.map(item => (
                 <ItemDisplay
